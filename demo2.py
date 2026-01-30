fb0e6e980108e6350ff9a7297c7cb35c2fb90e7c
totowang-hhh <tt1224@hotmail.com>
2026-01-30 16:41:15 +0800

    Fix translucent setting and float validation

--- demo2.py
+++ demo2.py
@@ -18,9 +18,11 @@ import random
 
 import re
 
-from PIL import Image, ImageFilter, ImageDraw
+
 
 from editable_treeview import EditableTreeview
+
+from liquidglass import LiquidGlass
 
 class Config:
     SHOW_GLASS_TOPOGRAPHY = False
@@ -152,18 +154,18 @@ def load_image(path):
     image = resize(image)
     win.blit(image, (0,0))
 
-def construct_blocks(config):
+def construct_blocks(window, config):
     glass_blocks: list[LiquidGlass] = []
     for block_args in config:
-        glass_blocks.append(LiquidGlass(
+        glass_blocks.append(LiquidGlass(window, 
             block_args[1], block_args[2], block_args[3], block_args[4], block_args[5], 
-            block_args[6], block_args[7], ))
+            block_args[6], block_args[7], block_args[8], block_args[9]))
     return glass_blocks
 
 def draw_all():
-    global glass_blocks_conf, curr_img_path, option_buttons
+    global win, glass_blocks_conf, curr_img_path, option_buttons
     load_image(curr_img_path) # Reload image first to cover any proviously drawn stuff
-    glass_blocks = construct_blocks(glass_blocks_conf)
+    glass_blocks = construct_blocks(win, glass_blocks_conf)
     for block in glass_blocks:
         block.render()
     draw_options(option_buttons=option_buttons) # Draw options finally to get it on top
@@ -358,7 +360,7 @@ class EditWindow(tk.Toplevel):
                                 self.select_field(block_index, index, silent=True)
                                 return
                         case "float":
-                            if attr.isdigit():
+                            if attr.replace('.', '', 1).isdigit():
                                 result.append(float(attr))
                             else:
                                 self.set_state(f"Value '{attr}' for {self.OPTION_NAMES[index]} "
@@ -431,167 +433,6 @@ class EditWindow(tk.Toplevel):
         self.curr_config[self.selected_block_index][self.option_entries.index(editing_row)] = \
             after_value
 
-class LiquidGlass():
-
-    def __init__(
-            self, 
-            x: int, 
-            y: int, 
-            w: int, 
-            h: int, 
-            z: int = 15, 
-            radius: int = 15, 
-            blur: int = 2, 
-            background: str = "#ffffff", 
-            alpha: float = 0.3
-            ):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.z = z
-        self.radius = radius
-        self.blur = blur
-        bg_rgb = hex_to_rgb(background)
-        self.translucent = (bg_rgb[0], bg_rgb[1], bg_rgb[2], int(alpha * 255))
-
-    def draw_rect(self):
-        pygame.draw.rect(win, (100, 100, 100), 
-                         (self.x, self.y, self.w, self.h), 
-                         1, border_radius=self.radius)
-
-    def calc_distance_to_edge(self, point: tuple[int, int]) -> list[int]:
-        # Handle edge: The rectangle shaped edge where the code handles
-        # Shape edge: The actual shape where the glass block takes up
-        rect_size = [self.w, self.h]
-        result: list[int] = [0, 0]
-        for direction, d_handle_neighbor in enumerate(point): 
-            # (var) direction: int, either 0 for x or 1 for y
-            # (expression) 1 - direction: mask (invert) the direction value to indicate direction 
-            #                             of the calculation result
-            # The coords of the point is the distance to handle edge.
-            # For such distance in each direction, we sort them into 3 cases to treat in different 
-            # way when getting the distance to the shape edge in the other direction (e.g. 
-            # d_handle in x => Recognized as case A => d_shape in y): 
-            d_handle = point[1-direction]
-            if self.radius <= d_handle_neighbor <= (rect_size[direction] - self.radius):
-                # Case 1: in middle rect part
-                # Shape edge is the handle edge
-                result[1-direction] = min(d_handle, rect_size[1-direction] - d_handle)
-            else:
-                # Case 2 & 3: Point is in either edge in such direction (within round corner region)
-                # |- Case 2: Left / Upper edges
-                # |- Case 3: Right / Lower edges
-                # Treat round corner regions region as a segment (1/4 circle), we convert them into 
-                # case 3 to make calculations easier
-                # (var) d_edge: distance of a point to edge of inner rectangle area
-                if d_handle_neighbor > (rect_size[direction] - d_handle_neighbor):
-                    d_handle_neighbor_abs = rect_size[direction] - d_handle_neighbor
-                else:
-                    d_handle_neighbor_abs = d_handle_neighbor
-                d_neighbor_edge = (self.radius - d_handle_neighbor_abs)
-                d_edge = min(d_handle, (rect_size[1-direction] - d_handle)) - self.radius
-                d_shape = d_edge + (self.radius ** 2 - d_neighbor_edge ** 2) ** 0.5
-                result[1-direction] = d_shape
-        # result.append((result[0] ** 2 + result[1] ** 2) ** 0.5) # Shortest straight line distance
-        return result
-
-    def calc_deflection_offset(self, distance_to_edge:int, max_offset: int) -> int:
-        if not 0 < distance_to_edge < self.z:
-            return 0
-        # point_z_height = (2 * self.z * distance_to_edge - distance_to_edge ** 2) ** 0.5
-        offset = (max_offset / self.z ** 4) * (distance_to_edge - self.z) ** 4
-        offset = int(round(offset, 0))
-        return offset
-
-    def render(self):
-        """This is used to draw or update the liquid glass block"""
-        global win
-        ## Load self config
-        base_x = self.x
-        base_y = self.y
-        width = self.w
-        height = self.h
-        rect_radius = self.radius
-        blur_radius = self.blur
-        ## Basic backdrop blur, written by GitHub Copilot
-        rect = pygame.Rect(base_x, base_y, width, height)
-        bg_surface = win.subsurface(rect).copy()
-        arr = pygame.surfarray.array3d(bg_surface)
-        arr = np.transpose(arr, (1, 0, 2))
-        pil_img = Image.fromarray(arr)
-        # Blur
-        pil_img = pil_img.filter(ImageFilter.GaussianBlur(radius=blur_radius))
-        # Round corner mask
-        mask_img = Image.new("L", (width, height), 0)
-        draw = ImageDraw.Draw(mask_img)
-        draw.rounded_rectangle([0, 0, width-1, height-1], radius=rect_radius, fill=255)
-        pil_img.putalpha(mask_img)
-        # Convert to pygame surface and draw
-        arr_blur = np.array(pil_img)
-        blur_surface = pygame.image.frombuffer(arr_blur.tobytes(), 
-                                               (width, height), "RGBA").convert_alpha()
-        win.blit(blur_surface, (base_x, base_y))
-        ## Deflection, the key part must be self-written :)
-        pixels: list[list[tuple]] = []
-        # Add calculated blurred region to a pixel array
-        for y in range(height):
-            pixels.append([])
-            for x in range(width):
-                pixels[y].append(tuple(win.get_at((base_x+x,base_y+y))))
-        # Calculate deflection
-        pixels_origin: list[list[tuple]] = pixels.copy()
-        for y in range(len(pixels)):
-            for x in range(len(pixels[y])):
-                pixel_handled = [False, False]
-                # x and y after deflection
-                after_x = x
-                after_y = y
-                # Calc distance to edge
-                distance_to_edge = self.calc_distance_to_edge((x, y))
-                if distance_to_edge[0] > 0 and distance_to_edge[1] > 0: 
-                    # ↑ This means that the pixel is within the shape
-                    # Calc after x and y base on distance to edge
-                    x_offset = self.calc_deflection_offset(distance_to_edge[0], self.w)
-                    y_offset = self.calc_deflection_offset(distance_to_edge[1], self.h)
-                    # If is right / bottom side, negative the offset
-                    if x > (self.w - self.z):
-                        x_offset = - x_offset
-                    if y > (self.h - self.z):
-                        y_offset = - y_offset
-                    after_x += x_offset
-                    after_y += y_offset
-                    # Correct out of range destinate x and y values
-                    after_x = int(get_between(after_x, 0, self.w - 1))
-                    after_y = int(get_between(after_y, 0, self.h - 1))
-                    # Handle the offset
-                    pixels[y][x] = pixels_origin[after_y][after_x]
-                if Config.SHOW_GLASS_TOPOGRAPHY: 
-                    approx_pixel_z_height = distance_to_edge[2]
-                    approx_pixel_z_height = get_between(approx_pixel_z_height, 0, 1)
-                    pixels[y][x] = (int(255*approx_pixel_z_height),
-                                    int(255-255*approx_pixel_z_height),0,255)
-        # Draw deflection
-        for y in range(len(pixels)):
-            for x in range(len(pixels[y])):
-                try:
-                    win.set_at((base_x+x, base_y+y), pixels[y][x])
-                except:
-                    print(f"Invalid pixel color @ ({base_x+x},{base_y+y}) "
-                           "with RGB(A) {pixels[y][x]}")
-                    win.set_at((base_x+x, base_y+y), (155,0,255,255))
-        ## Translucent layer
-        overlay = pygame.Surface((self.w, self.h), pygame.SRCALPHA)
-        pygame.draw.rect(
-            overlay, 
-            self.translucent, 
-            pygame.Rect(0, 0, self.w, self.h), 
-            border_radius = self.radius
-            )
-        win.blit(overlay, (self.x, self.y))
-        ## Finally draw the outer frame
-        self.draw_rect()
-
 
 class Test():
     @staticmethod
@@ -600,7 +441,7 @@ class Test():
         for y, line in enumerate(test_matrix):
             for x, point in enumerate(line):
                 test_matrix[y][x] = [int(item) for item in LiquidGlass.calc_distance_to_edge(\
-                    LiquidGlass(0, 0, w, h, radius=r), (x, y))]
+                    LiquidGlass(pygame.Surface((w, h)), 0, 0, w, h, radius=r), (x, y))]
             print(line)
         return test_matrix
 
@@ -618,10 +459,13 @@ edit_window = EditWindow()
 options = {
     "Apple Liquid Glass Effect in Pygame: Demo 2 (Playground)": \
         lambda: webbrowser.open("https://github.com/TotoWang-hhh/AppleGlassEffect/"),
-    "Moving glass block (Test rendering speed)": None,
+    "Duplicatable glass blocks & Faster rendering": None,
     "2025 by rgzz666": lambda: webbrowser.open("https://github.com/TotoWang-hhh"),
-    "Load image": lambda: change_image(filebox.askopenfilename\
-                                        (title="Select an image to use as background")),
+    "Load image": \
+        lambda: change_image(filebox.askopenfilename(
+            title="Select an image to use as background")),
+    "Open edit window": edit_window.show,
+    "Redraw all": draw_all,
     }
 option_buttons = make_option_buttons(win, options)
 
--- demo3.py
+++ demo3.py
@@ -19,7 +19,7 @@ import random
 import re
 import time
 
-from PIL import Image, ImageFilter, ImageDraw
+from liquidglass import LiquidGlass
 
 
 class Config:
@@ -254,149 +254,6 @@ def resize(image):
     win_h = new_h
     return resized_img
 
-
-class LiquidGlass():
-
-    def __init__(
-            self, 
-            parent: pygame.Surface, 
-            x: int, 
-            y: int, 
-            w: int, 
-            h: int, 
-            z: int = 15, 
-            radius: int = 15, 
-            blur: int = 2, 
-            background: str = "#ffffff", 
-            alpha: float = 0.2
-            ):
-        self.parent = parent
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.z = z
-        self.radius = radius
-        self.blur = blur
-        bg_rgb = hex_to_rgb(background)
-        self.translucent = (bg_rgb[0], bg_rgb[1], bg_rgb[2], int(alpha * 255))
-
-    def draw_rect(self):
-        pygame.draw.rect(win, (100, 100, 100), 
-                         (self.x, self.y, self.w, self.h), 
-                         1, border_radius=self.radius)
-
-    def calc_distance_to_edge(self, point: tuple[int, int]) -> list[int]:
-        # Handle edge: The rectangle shaped edge where the code handles
-        # Shape edge: The actual shape where the glass block takes up
-        rect_size = [self.w, self.h]
-        result: list[int] = [0, 0]
-        for direction, d_handle_neighbor in enumerate(point): 
-            # (var) direction: int, either 0 for x or 1 for y
-            # (expression) 1 - direction: mask (invert) the direction value to indicate direction 
-            #                             of the calculation result
-            # The coords of the point is the distance to handle edge.
-            # For such distance in each direction, we sort them into 3 cases to treat in different 
-            # way when getting the distance to the shape edge in the other direction (e.g. 
-            # d_handle in x => Recognized as case A => d_shape in y): 
-            d_handle = point[1-direction]
-            if self.radius <= d_handle_neighbor <= (rect_size[direction] - self.radius):
-                # Case 1: in middle rect part
-                # Shape edge is the handle edge
-                result[1-direction] = min(d_handle, rect_size[1-direction] - d_handle)
-            else:
-                # Case 2 & 3: Point is in either edge in such direction (within round corner region)
-                # |- Case 2: Left / Upper edges
-                # |- Case 3: Right / Lower edges
-                # Treat round corner regions region as a segment (1/4 circle), we convert them into 
-                # case 3 to make calculations easier
-                # (var) d_edge: distance of a point to edge of inner rectangle area
-                if d_handle_neighbor > (rect_size[direction] - d_handle_neighbor):
-                    d_handle_neighbor_abs = rect_size[direction] - d_handle_neighbor
-                else:
-                    d_handle_neighbor_abs = d_handle_neighbor
-                d_neighbor_edge = (self.radius - d_handle_neighbor_abs)
-                d_edge = min(d_handle, (rect_size[1-direction] - d_handle)) - self.radius
-                d_shape = d_edge + (self.radius ** 2 - d_neighbor_edge ** 2) ** 0.5
-                result[1-direction] = d_shape
-        # result.append((result[0] ** 2 + result[1] ** 2) ** 0.5) # Shortest straight line distance
-        return result
-
-    def calc_deflection_offset(self, distance_to_edge:int, max_offset: int) -> int:
-        if not 0 < distance_to_edge < self.z:
-            return 0
-        # point_z_height = (2 * self.z * distance_to_edge - distance_to_edge ** 2) ** 0.5
-        offset = (max_offset / self.z ** 4) * (distance_to_edge - self.z) ** 4
-        offset = int(round(offset, 0))
-        return offset
-
-    def render(self):
-        """This is used to draw or update the liquid glass block"""
-        ## Basic backdrop blur, written by GitHub Copilot
-        rect = pygame.Rect(self.x, self.y, self.w, self.h)
-        bg_surface = self.parent.subsurface(rect).copy()
-        arr = pygame.surfarray.array3d(bg_surface)
-        arr = np.transpose(arr, (1, 0, 2))
-        if self.blur != 0:
-            pil_img = Image.fromarray(arr)
-            # Blur
-            pil_img = pil_img.filter(ImageFilter.GaussianBlur(radius=self.blur))
-            # Round corner mask
-            mask_img = Image.new("L", (self.w, self.h), 0)
-            draw = ImageDraw.Draw(mask_img)
-            draw.rounded_rectangle([0, 0, self.w-1, self.h-1], radius=self.radius, fill=255)
-            pil_img.putalpha(mask_img)
-            # Convert to pygame surface and draw
-            arr = np.array(pil_img)
-        else:
-            arr =arr
-        ## Deflection, the key part must be self-written :)
-        pixels_origin: list[list[tuple]] = arr.copy()
-        for y in range(len(arr)):
-            for x in range(len(arr[y])):
-                # x and y after deflection
-                after_x = x
-                after_y = y
-                # Calc distance to edge
-                distance_to_edge = self.calc_distance_to_edge((x, y))
-                if distance_to_edge[0] > 0 and distance_to_edge[1] > 0: 
-                    # ↑ This means that the pixel is within the shape
-                    # Calc after x and y base on distance to edge
-                    x_offset = self.calc_deflection_offset(distance_to_edge[0], self.w)
-                    y_offset = self.calc_deflection_offset(distance_to_edge[1], self.h)
-                    # If is right / bottom side, negative the offset
-                    if x > (self.w - self.z):
-                        x_offset = - x_offset
-                    if y > (self.h - self.z):
-                        y_offset = - y_offset
-                    after_x += x_offset
-                    after_y += y_offset
-                    # Correct out of range destinate x and y values
-                    after_x = int(get_between(after_x, 0, self.w - 1))
-                    after_y = int(get_between(after_y, 0, self.h - 1))
-                    # Handle the offset
-                    arr[y][x] = pixels_origin[after_y][after_x]
-                if Config.SHOW_GLASS_TOPOGRAPHY: 
-                    approx_pixel_z_height = distance_to_edge[2]
-                    approx_pixel_z_height = get_between(approx_pixel_z_height, 0, 1)
-                    arr[y][x] = (int(255*approx_pixel_z_height),
-                                    int(255-255*approx_pixel_z_height),0,255)
-        # Draw deflection
-        handled_surface = pygame.image.frombuffer(arr.tobytes(), (self.w, self.h), "RGB").convert_alpha()
-        self.parent.blit(handled_surface, (self.x, self.y))
-        ## Translucent layer
-        overlay = pygame.Surface((self.w, self.h), pygame.SRCALPHA)
-        pygame.draw.rect(
-            overlay, 
-            self.translucent, 
-            pygame.Rect(0, 0, self.w, self.h), 
-            border_radius = self.radius
-            )
-        self.parent.blit(overlay, (self.x, self.y))
-        ## Finally draw the outer frame
-        self.draw_rect()
-
-
 class Test():
     @staticmethod
     def calc_distance_edge(w: int, h: int, r: int) -> list[list[list[int]]]:
@@ -404,7 +261,7 @@ class Test():
         for y, line in enumerate(test_matrix):
             for x, point in enumerate(line):
                 test_matrix[y][x] = [int(item) for item in LiquidGlass.calc_distance_to_edge(\
-                    LiquidGlass(0, 0, w, h, radius=r), (x, y))]
+                    LiquidGlass(pygame.Surface((w, h)), 0, 0, w, h, radius=r), (x, y))]
             print(line)
         return test_matrix
 
