aa40cca3cc1a3e12af9f15e80d37f76f9b263be1
totowang-hhh <tt1224@hotmail.com>
2026-02-02 19:53:27 +0800

    Icons & Auto build exe script for window

---
+++ build_exe.bat
@@ -0,0 +1,22 @@
+echo Building executables...
+pyinstaller -i ./icons/icon1.ico  -w demo1.py
+pyinstaller -i ./icons/icon2.ico  -w demo2.py
+pyinstaller -i ./icons/icon3.ico  -w demo3.py
+echo Copying files...
+mkdir ".\finals\"
+copy ".\dist\demo1\demo1.exe" ".\finals\Old Method (Demo 1).exe"
+copy ".\dist\demo2\demo2.exe" ".\finals\Playground (Demo 2).exe"
+copy ".\dist\demo3\demo3.exe" ".\finals\Read-time Rendering (Demo 3).exe"
+mkdir ".\finals\_internal\"
+copy ".\dist\demo2\_internal\*" ".\finals\_internal\"
+mkdir ".\finals\icons\"
+copy ".\icons\*" ".\finals\icons\"
+mkdir ".\finals\test_imgs\"
+copy ".\test_imgs\*" ".\finals\test_imgs\"
+copy ".\README.md*" ".\finals"
+echo Cleaning up...
+rmdir /s .\dist\
+rmdir /s .\build\
+echo ====================
+echo Build completed
+pause
